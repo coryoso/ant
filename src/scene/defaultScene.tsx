06288b0b6b91afa3a3f3e4b25ba5c29a59bd8d98
Cornelius Carl <corneliuscarl@me.com>
2023-11-11 16:44:07 +0100

    feat: move behaviour

--- src/scene/defaultScene.tsx
+++ src/scene/defaultScene.tsx
@@ -1,17 +1,49 @@
-import { Environment, OrbitControls } from "@react-three/drei"
+import {
+	Box,
+	Dodecahedron,
+	Environment,
+	OrbitControls,
+} from "@react-three/drei"
+import { useFrame } from "@react-three/fiber"
+import AntAgent from "../entities/antAgent"
+import { useEnvironment } from "../store/environment"
 
 const DefaultScene = () => {
+	const agents = useEnvironment((state) =>
+		state.agentSystems.map((system) => system.agents).flat(),
+	)
+	const execute = useEnvironment((state) => state.execute)
+
+	useFrame(({ clock }) => {
+		execute()
+	})
+
 	return (
 		<>
 			<Environment preset="sunset" background />
 
 			<pointLight position={[10, 10, 10]} />
-			<mesh>
-				<sphereGeometry />
-				<meshStandardMaterial roughness={0.1} color="hotpink" />
-			</mesh>
+			<directionalLight position={[0, 10, 0]} intensity={0.5} />
+
+			<Box position={[-10, 0, 0]}>
+				<meshStandardMaterial color="orange" />
+			</Box>
+			<Box position={[10, 0, 0]}>
+				<meshStandardMaterial color="green" />
+			</Box>
+
+			<Dodecahedron position={[-5, 2.5, 0]}>
+				<meshStandardMaterial color="hotpink" />
+			</Dodecahedron>
+			<Dodecahedron position={[5, -2.5, 0]}>
+				<meshStandardMaterial color="hotpink" />
+			</Dodecahedron>
 
 			<OrbitControls />
+
+			{agents.map((agent) => (
+				<AntAgent position={agent.position} />
+			))}
 		</>
 	)
 }
